d757243ddeed529b438d7fc108f38db9922bed28
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-02-27 16:31:21 -0300

    COMECEI O JOGO DA VELHA
    Comecei adicionando um foreach no tabuleiro_marc onde depois adicionei um envento em click, para adicionar cada o "x" ou o "o" em cada parte, agora eu irei começar a fazer a logica do meu programa.

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,27 @@
 let jogador1 = 'X'
 let jogador2 = 'O'
+let jogador1_select11 , jogador2_select22
+let valor_jogador = [], valor, contador
 let tabuleiro_marc = document.querySelectorAll('.tabuleiro_marc')
+let c = 0
 
+tabuleiro_marc.forEach((tabuleiro)=>{
+    tabuleiro.addEventListener("click", ()=>{
+        if(tabuleiro.innerHTML ==  ""){
+            if (c==0){
+                jogador1_select11 = true
+                jogador2_select22 = false
+                valor_jogador.push(tabuleiro.getAttribute("data-tab"))
+    
+               setTimeout(()=>{
+                c++
+               },"100")
+            }
+           
+        }
+        
+    })
+})
 function jogo(){
     
 }
